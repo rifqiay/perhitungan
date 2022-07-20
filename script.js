783ce7db501b2595605ec1da67d7796aa6bc5b35
rifqiay <muhammadrifqiay5@gmail.com>
2022-07-20 21:36:42 +0700

    menambah fitur set target

--- script.js
+++ script.js
@@ -2,23 +2,51 @@ const display = document.querySelector(".index");
 const tambah = document.querySelector(".tambah");
 const kurang = document.querySelector(".kurang");
 const reset = document.querySelector(".reset");
+let input = document.querySelector(".input");
+const setTarget = document.querySelector(".setTarget");
+let displayTarget = document.querySelector(".display-target");
 let counter = 0;
+let setCounter;
 
-tambah.addEventListener("click", () => {
+function add() {
   counter++;
   window.navigator.vibrate(100);
   display.innerHTML = counter;
-});
+}
 
-kurang.addEventListener("click", () => {
+const min = () => {
   if (counter > 0) {
     counter--;
     window.navigator.vibrate(100);
     display.innerHTML = counter;
   }
+};
+
+tambah.addEventListener("click", () => {
+  if (counter + 1 == setCounter) {
+    console.log(counter + 1);
+    display.innerHTML = counter + 1;
+    window.navigator.vibrate(5000);
+    return;
+  }
+  add();
+});
+
+kurang.addEventListener("click", () => {
+  min();
 });
 
 reset.addEventListener("click", () => {
   counter *= 0;
   display.innerHTML = counter;
+  displayTarget.innerHTML = "";
+  window.navigator.vibrate(0);
+  setCounter = null;
+});
+
+setTarget.addEventListener("click", () => {
+  let getInput = input.value;
+  setCounter = getInput;
+  input.value = "";
+  displayTarget.innerHTML = setCounter;
 });
